03cb874d7740303682d856e5420f324d8253c1d1
Sufiyan Shaikh <sksufiyan128@gmail.com>
2023-09-20 14:00:52 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import Navbar from './Components/Navbar';
+import TextForm from './Components/TextForm';
+ 
+
+function App() {
+  return (
+    <>
+    {/* <Navbar title="TextUtils" aboutText="About Textutils" /> */}
+    {/*Navbar/> */}
+    <Navbar title="Textutils"/>
+    <div className = "container my-3">
+    <TextForm heading="Enter the text to analyze below"/>
+    </div>
+    </>
+  );
+}
+
+export default App;
+
+
+
